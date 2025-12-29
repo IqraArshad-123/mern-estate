8cda8d225c83d7e4287a05845f3406e65124d6f9
IqraArshad-123 <iqraa6244@gmail.com>
2025-12-29 18:48:28 +0500

    complete upload listing images create listing page get listing api route user listing functionality

--- api/routes/upload.route.js
+++ api/routes/upload.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import multer from "multer";
-import { uploadAvatar } from "../controllers/upload.controller.js";
+import { uploadAvatar, uploadListingImages } from "../controllers/upload.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
@@ -11,5 +11,52 @@ const upload = multer({
 });
 
 router.post("/", verifyToken, upload.single("file"), uploadAvatar);
+router.post("/listing-images", verifyToken, upload.array("images", 6), uploadListingImages)
 
 export default router;
+
+// import express from "express";
+// import multer from "multer";
+// import {
+//   uploadAvatar,
+//   uploadListingImages,
+// } from "../controllers/upload.controller.js";
+// import { verifyToken } from "../utils/verifyUser.js";
+
+// const router = express.Router();
+// const upload = multer({ storage: multer.memoryStorage() });
+
+// router.post(
+//   "/avatar",
+//   verifyToken,
+//   upload.single("avatar"),
+//   uploadAvatar
+// );
+
+// router.post(
+//   "/listing-images",
+//   verifyToken,
+//   upload.array("images", 6),
+//   uploadListingImages
+// );
+
+// export default router;
+
+// import express from "express";
+// import multer from "multer";
+// import {uploadAvatar, uploadListingImages } from "../controllers/upload.controller.js";
+
+// const router = express.Router();
+// const upload = multer({ storage: multer.memoryStorage() });
+
+
+// router.post("/avatar", upload.single("avatar"), uploadAvatar);
+
+// // THIS IS THE FIX → Route must match frontend fetch
+// router.post(
+//   "/listing-images", // <-- exact same as frontend fetch
+//   upload.array("images", 6), 
+//   uploadListingImages
+// );
+
+// export default router;
